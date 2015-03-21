06186c3e0e94a65519719121b7454b7cfb5150a5
Bilou84 <nicolas.gauthier84@gmail.com>
2015-03-21 15:36:15 +0100

    tileMap: Add TileSet.setTileProperty
    --HG--
    branch : plugins

--- plugins/sparklinlabs/tileMap/api/Sup.TileSet.d.ts
+++ plugins/sparklinlabs/tileMap/api/Sup.TileSet.d.ts
@@ -1,5 +1,6 @@
 declare module Sup {
   class TileSet extends Asset {
     getTileProperties(tile: number): {[key:string]: string;}
+    setTileProperty(tile: number, propertyName: string, propertyValue: string)
   }
 }
--- plugins/sparklinlabs/tileMap/api/Sup.TileSet.ts
+++ plugins/sparklinlabs/tileMap/api/Sup.TileSet.ts
@@ -9,5 +9,18 @@ module Sup {
       properties = (properties) ? properties : {}
       return properties
     }
+    setTileProperty(tile, propertyName, propertyValue) {
+      var tilesPerRow = this.__inner.data.texture.image.width / this.__inner.data.gridSize;
+
+      var x = tile % tilesPerRow;
+      var y = window.Math.floor(tile / tilesPerRow);
+      var properties = this.__inner.data.tileProperties[x + "_" + y];
+      properties = (properties) ? properties : {};
+      if ( propertyValue != null ) {
+        this.__inner.data.tileProperties[x + "_" + y][propertyName] = propertyValue;
+      } else if (this.__inner.data.tileProperties[x + "_" + y][propertyName] != null ) {
+        delete this.__inner.data.tileProperties[x + "_" + y][propertyName]
+      }
+    }
   }
 }
