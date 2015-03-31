975279c3d80ac740ee7ab4ba1216e4cd51985c75
Elisée Maurer <elisee@sparklinlabs.com>
2015-03-31 16:12:48 +0200

    typescript: Add list of valid key names in Sup.Input.d.ts
    --HG--
    branch : plugins

--- plugins/sparklinlabs/typescript/api/Sup.Input.d.ts
+++ plugins/sparklinlabs/typescript/api/Sup.Input.d.ts
@@ -17,6 +17,15 @@ declare module Sup {
     function vibrate(pattern: number): void;
     function vibrate(pattern: Array<number>): void;
 
+    // Valid key names (based on window.KeyEvent.DOM_VK_*):
+    // CANCEL, HELP, BACK_SPACE, TAB, CLEAR, RETURN, ENTER, SHIFT, CONTROL, ALT,
+    // PAUSE, CAPS_LOCK, ESCAPE, SPACE, PAGE_UP, PAGE_DOWN, END, HOME,
+    // LEFT, UP, RIGHT, DOWN, PRINTSCREEN, INSERT, DELETE,
+    // 0 to 9, SEMICOLON, EQUALS, A to Z,
+    // CONTEXT_MENU, NUMPAD0 to NUMPAD9,
+    // MULTIPLY, ADD, SEPARATOR, SUBTRACT, DECIMAL, DIVIDE,
+    // F1 to F24, NUM_LOCK, SCROLL_LOCK, COMMA, PERIOD, SLASH, BACK_QUOTE,
+    // OPEN_BRACKET, BACK_SLASH, CLOSE_BRACKET, QUOTE, META
     function isKeyDown(keyName: string): boolean;
     function wasKeyJustPressed(keyName: string): boolean;
     function wasKeyJustReleased(keyName: string): boolean;
