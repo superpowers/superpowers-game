7057f6cebb3f9c2ab329ed58d0cfce1cea666d0b
Ralmn <contact.ralmn@gmail.com>
2015-04-25 00:01:08 +0200

    rngjs: Port to Typescript
    --HG--
    branch : plugins

--- plugins/sparklinlabs/rngjs/api/index.coffee
+++
@@ -1,5 +0,0 @@
-fs = require 'fs'
-
-SupAPI.registerPlugin 'typescript', 'rng', {
-  defs: fs.readFileSync(__dirname + '/rng.d.ts.txt', encoding: 'utf8')
-}
---
+++ plugins/sparklinlabs/rngjs/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require("gulp");
+var tasks = [];
+
+
+
+// Typescript
+var ts = require('gulp-typescript');
+gulp.task("typescript", function() {
+  var tsResult = gulp.src(["**/*.ts", "!node_modules/**"]).pipe(ts({
+    typescript: require("typescript"),
+    noImplicitAny: true,
+    declarationFiles: false,
+    module: "commonjs",
+    target: "ES5"
+  }));
+  return tsResult.js.pipe(gulp.dest("./"));
+});
+
+// Browserify
+var browserify = require("browserify");
+var vinylSourceStream = require("vinyl-source-stream");
+function makeBrowserify(source, destination, output) {
+  gulp.task(output + "-browserify", ["typescript"], function() {
+    var bundler = browserify(source);
+    bundler.transform("brfs");
+    function bundle() { return bundler.bundle().pipe(vinylSourceStream(output + ".js")).pipe(gulp.dest(destination)); };
+    return bundle();
+  });
+  tasks.push(output + "-browserify");
+}
+
+makeBrowserify("./api/index.js", "./public", "api");
+
+// All
+gulp.task("default", tasks);
