69beca34a9eafb700bfcfa5dd3141b1a5ce9b926
Elisée Maurer <elisee@sparklinlabs.com>
2015-04-25 01:37:47 +0200

    sprite: Restore SpriteRendererEditor.onEntryTrashed's signature
    --HG--
    branch : plugins

--- plugins/sparklinlabs/sprite/componentEditors/SpriteRendererEditor.ts
+++ plugins/sparklinlabs/sprite/componentEditors/SpriteRendererEditor.ts
@@ -82,7 +82,7 @@ class SpriteRendererEditor {
     if (id !== this.spriteAssetId) return;
     this.spriteTextField.value = this.projectClient.entries.getPathFromId(this.spriteAssetId);
   }
-  onEntryTrashed(entry: any, path: string) {}
+  onEntryTrashed(id: string) {}
 
   onAssetReceived(assetId: string, asset: any) {
     if (assetId != this.spriteAssetId) return;
