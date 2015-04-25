95a66ea602b706d32bc76298f781f793c7b03506
Elisée Maurer <elisee@sparklinlabs.com>
2015-04-25 10:43:22 +0200

    sprite: Replace a couple left-over "var"s
    --HG--
    branch : plugins

--- plugins/sparklinlabs/sprite/runtime/SpriteRenderer.ts
+++ plugins/sparklinlabs/sprite/runtime/SpriteRenderer.ts
@@ -1,6 +1,6 @@
 export function setupComponent(player: SupRuntime.Player, component: any, config: any) {
   if (config.spriteAssetId != null) {
-    var sprite = player.getOuterAsset(config.spriteAssetId).__inner;
+    let sprite = player.getOuterAsset(config.spriteAssetId).__inner;
     component.setSprite(sprite);
 
     if (config.animationId != null) {
