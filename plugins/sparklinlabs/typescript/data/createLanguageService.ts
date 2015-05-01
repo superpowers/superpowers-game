34a483daced3f86dd7f551915b8e367375aab84d
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-02 00:28:47 +0200

    typescript: Always use ES5 target when compiling TypeScript
    --HG--
    branch : plugins

--- plugins/sparklinlabs/typescript/data/createLanguageService.ts
+++ plugins/sparklinlabs/typescript/data/createLanguageService.ts
@@ -1,6 +1,8 @@
 import * as ts from "typescript";
 
 export default function createLanguageService(fileNames: string[], files: {[name: string]: string}, libSource: string, compilerOptions: ts.CompilerOptions={}) {
+  if (compilerOptions.target == null) compilerOptions.target = ts.ScriptTarget.ES5;
+
   fileNames.splice(0, 0, "lib.d.ts");
   // Create the language service host to allow the LS to communicate with the host
   const servicesHost: ts.LanguageServiceHost = {
