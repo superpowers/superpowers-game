bd5008facbd86b80aae304686554b7ebb7531158
Elisée Maurer <elisee@sparklinlabs.com>
2015-05-01 09:33:41 +0200

    scene: Cast global to any since it's now typed and add comment about requiring SupEngine
    --HG--
    branch : plugins

--- plugins/sparklinlabs/scene/data/SceneAsset.ts
+++ plugins/sparklinlabs/scene/data/SceneAsset.ts
@@ -1,6 +1,9 @@
 let serverRequire = require;
 let THREE: any;
-if (global.window == null) THREE = serverRequire("../../../../system/SupEngine").THREE;
+// NOTE: It is important that we require THREE through SupEngine
+// so that we inherit any settings, like the global Euler order
+// (or, alternatively, we could duplicate those settings...)
+if ((<any>global).window == null) THREE = serverRequire("../../../../system/SupEngine").THREE;
 
 import * as path from "path";
 import * as fs from "fs";
