d9fcc501cd7323d1dedbc4cf0fc8437537cb3419
Bilou84 <nicolas.gauthier84@gmail.com>
2015-05-07 10:43:13 +0200

    gameSettings: Make sure both ratio numerator and denominator are defined to be applied
    --HG--
    branch : plugins

--- plugins/sparklinlabs/gameSettings/runtime/gameSettings.ts
+++ plugins/sparklinlabs/gameSettings/runtime/gameSettings.ts
@@ -2,7 +2,7 @@ export function init(player: SupRuntime.Player, callback: Function) {
   SupEngine.GameInstance.framesPerSecond = player.resources.gameSettings.framesPerSecond;
   SupRuntime.Player.updateInterval = 1 / SupEngine.GameInstance.framesPerSecond * 1000;
 
-  if (player.resources.gameSettings.ratioNumerator != null) {
+  if (player.resources.gameSettings.ratioNumerator != null && player.resources.gameSettings.ratioDenominator != null) {
     player.gameInstance.setRatio(player.resources.gameSettings.ratioNumerator / player.resources.gameSettings.ratioDenominator);
   }
   callback();
