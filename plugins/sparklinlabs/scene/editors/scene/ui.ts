b05753027cdeac1d16fe06c0854932fa6dd2fd99
Elisée Maurer <elisee@sparklinlabs.com>
2015-08-14 16:06:08 +0200

    scene: Add missing semicolon
    --HG--
    branch : plugins

--- plugins/sparklinlabs/scene/editors/scene/ui.ts
+++ plugins/sparklinlabs/scene/editors/scene/ui.ts
@@ -159,7 +159,7 @@ function onNodeDrop(dropInfo: any, orderedNodes: any) {
     socket.emit("edit:assets", info.assetId, "moveNode", id, dropPoint.parentId, dropPoint.index + i, (err: string) => { if (err != null) alert(err); });
     if (! sameParent || sourceChildren.indexOf(data.sceneUpdater.sceneAsset.nodes.byId[id]) >= dropPoint.index) i++;
   }
-  return false
+  return false;
 }
 
 function onNodeActivate() { ui.nodesTreeView.selectedNodes[0].classList.toggle("collapsed"); }
