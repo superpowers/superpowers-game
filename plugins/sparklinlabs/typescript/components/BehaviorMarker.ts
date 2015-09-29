8450a12fc1e8a6d5eb6a38cc07e7229fd5f22213
Bilou84 <nicolas.gauthier84@gmail.com>
2015-09-29 16:05:10 +0200

    typescript: Add missing typeName for BehaviorMarker component
    --HG--
    branch : plugins

--- plugins/sparklinlabs/typescript/components/BehaviorMarker.ts
+++ plugins/sparklinlabs/typescript/components/BehaviorMarker.ts
@@ -3,5 +3,9 @@ import BehaviorUpdater from "./BehaviorUpdater";
 export default class BehaviorMarker extends SupEngine.ActorComponent {
   static Updater = BehaviorUpdater;
 
+  constructor(actor: SupEngine.Actor) {
+    super(actor, "BehaviorMarker");
+  }
+
   setIsLayerActive(active: boolean) {}
 }
