35ce7aa84f49c88e873472be8f7ea92123fe6798
Elisée Maurer <elisee@sparklinlabs.com>
2015-10-02 17:13:31 +0200

    SupEngine: Clean up Audio.ts

--- SupEngine/src/Audio.ts
+++ SupEngine/src/Audio.ts
@@ -4,13 +4,12 @@ export default class Audio {
 
   constructor() {}
   getContext(): AudioContext {
-    if ((<any>window)["AudioContext"] == null) return null
-
+    if ((<any>window)["AudioContext"] == null) return null;
     if (this._ctx != null) return this._ctx;
 
-    this._ctx = new AudioContext()
-    this.masterGain = this._ctx.createGain()
-    this.masterGain.gain.value = 1
+    this._ctx = new AudioContext();
+    this.masterGain = this._ctx.createGain();
+    this.masterGain.gain.value = 1;
     this.masterGain.connect(this._ctx.destination);
     return this._ctx;
   }
