aa1e359de90fd3591a2a0448a2a1fe5b3ad733aa
Bilou84 <nicolas.gauthier84@gmail.com>
2015-10-09 19:42:00 +0200

    arcadePhysics2D: Remove support of the actor scale Doesn't work properly and will cause too much trouble
    --HG--
    branch : plugins

--- plugins/sparklinlabs/arcadePhysics2D/components/ArcadeBody2D.ts
+++ plugins/sparklinlabs/arcadePhysics2D/components/ArcadeBody2D.ts
@@ -66,11 +66,9 @@ export default class ArcadeBody2D extends SupEngine.ActorComponent {
     this.type = "tileMap";
     this.tileMapAsset = config.tileMapAsset;
     this.tileSetAsset = config.tileSetAsset;
-    let scale = this.actor.getLocalScale(new THREE.Vector3());
-
     this.mapToSceneFactor = {
-      x: this.tileSetAsset.__inner.data.grid.width / this.tileMapAsset.__inner.data.pixelsPerUnit * scale.x,
-      y: this.tileSetAsset.__inner.data.grid.height / this.tileMapAsset.__inner.data.pixelsPerUnit * scale.y,
+      x: this.tileSetAsset.__inner.data.grid.width / this.tileMapAsset.__inner.data.pixelsPerUnit,
+      y: this.tileSetAsset.__inner.data.grid.height / this.tileMapAsset.__inner.data.pixelsPerUnit,
     }
     this.tileSetPropertyName = config.tileSetPropertyName;
     if (config.layersIndex != null) {
