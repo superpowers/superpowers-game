c8977b61eab775cf99fc7e8afeb329e64d3523b3
Bilou84 <nicolas.gauthier84@gmail.com>
2015-11-03 13:24:00 +0100

    light: Remove left over log
    --HG--
    branch : plugins

--- plugins/sparklinlabs/light/runtime/lightSettingsResource.ts
+++ plugins/sparklinlabs/light/runtime/lightSettingsResource.ts
@@ -1,8 +1,6 @@
 export function loadResource(player: SupRuntime.Player, resourceName: string, callback: (err: Error, data?: any) => any) {
-  console.log("load light");
   player.getAssetData(`resources/${resourceName}/resource.json`, `json`, (err, data) => {
     if (err != null) { callback(err); return; }
     callback(null, data);
-    console.log("light loaded");
   });
 }
