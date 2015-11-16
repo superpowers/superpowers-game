558cce6564870bec2e35d44d4b7d2cb5ce2397d4
Bilou84 <nicolas.gauthier84@gmail.com>
2015-11-16 23:42:51 +0100

    home: Use electron for the app
    --HG--
    branch : electron

--- plugins/sparklinlabs/home/editors/main/links.ts
+++ plugins/sparklinlabs/home/editors/main/links.ts
@@ -1,11 +1,11 @@
-let nwDispatcher = (<any>window).nwDispatcher;
-if (nwDispatcher != null) {
-  let gui = nwDispatcher.requireNwGui();
+let nodeRequire = require;
+//let shell = nodeRequire("shell");
 
+if (SupClient.isApp) {
   document.querySelector(".sidebar .links").addEventListener("click", (event: any) => {
     if (event.target.tagName !== "A") return;
 
     event.preventDefault();
-    gui.Shell.openExternal(event.target.href);
+    window.parent.postMessage({ type: "openLink", content: event.target.href }, window.location.origin)
   });
 }
