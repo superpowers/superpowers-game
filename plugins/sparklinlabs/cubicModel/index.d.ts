21109ecc6bbe768ad3c235a3ddfbe4a2884e0d98
Elisée Maurer <elisee@sparklinlabs.com>
2015-11-22 11:38:30 +0100

    Unify spacing for /// <ref ... />
    --HG--
    branch : plugins

--- plugins/sparklinlabs/cubicModel/index.d.ts
+++ plugins/sparklinlabs/cubicModel/index.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="../transformHelpers/helpers.d.ts"/>
-/// <reference path="../../../../../SupClient/SupClient.d.ts"/>
-/// <reference path="../../../../../SupCore/SupCore.d.ts"/>
-/// <reference path="../../../SupEngine/SupEngine.d.ts"/>
-/// <reference path="../../../SupRuntime/SupRuntime.d.ts"/>
+/// <reference path="../transformHelpers/helpers.d.ts" />
+/// <reference path="../../../../../SupClient/SupClient.d.ts" />
+/// <reference path="../../../../../SupCore/SupCore.d.ts" />
+/// <reference path="../../../SupEngine/SupEngine.d.ts" />
+/// <reference path="../../../SupRuntime/SupRuntime.d.ts" />
