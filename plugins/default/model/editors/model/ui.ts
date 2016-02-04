superpowers/superpowers-game

commit 66379222881924770b404f99ba29978990b8c7fb

plugins: Match changes to ProjectClient.editAsset and ProjectClient.editResource

plugins/default/model/editors/model/ui.ts
@@ -1,4 +1,4 @@
-import { data, editAsset } from "./network";
+import { data } from "./network";
 
 import importModel, { ImportLogEntry } from "./importers/index";
 import ModelAsset from "../../data/ModelAsset";
@@ -199,8 +199,8 @@ function onModelFileSelectChange(event: any) {
     setImportLog(log);
 
     if (data != null) {
-      editAsset("setModel", data.upAxisMatrix, data.attributes, data.bones);
-      if (data.maps != null) editAsset("setMaps", data.maps);
+      data.projectClient.editAsset(SupClient.query.asset, "setModel", data.upAxisMatrix, data.attributes, data.bones);
+      if (data.maps != null) data.projectClient.editAsset(SupClient.query.asset, "setMaps", data.maps);
     }
   });
 }
@@ -211,7 +211,7 @@ function onPrimaryMapFileSelectChange(event: Event) {
   ui.errorPaneStatus.classList.remove("has-errors");
 
   const reader = new FileReader;
-  reader.onload = (event) => { editAsset("setMaps", { map: reader.result }); };
+  reader.onload = (event) => { data.projectClient.editAsset(SupClient.query.asset, "setMaps", { map: reader.result }); };
 
   const element = <HTMLInputElement>event.target;
   reader.readAsArrayBuffer(element.files[0]);
@@ -249,15 +249,15 @@ function downloadTexture(textureName: string) {
   }
 }
 
-function onChangeFiltering(event: any) { editAsset("setProperty", "filtering", event.target.value); }
-function onChangeWrapping(event: any) { editAsset("setProperty", "wrapping", event.target.value); }
+function onChangeFiltering(event: any) { data.projectClient.editAsset(SupClient.query.asset, "setProperty", "filtering", event.target.value); }
+function onChangeWrapping(event: any) { data.projectClient.editAsset(SupClient.query.asset, "setProperty", "wrapping", event.target.value); }
 function onShowSkeletonChange(event: Event) { data.modelUpdater.modelRenderer.setShowSkeleton((<HTMLInputElement>event.target).checked); }
-function onChangeUnitRatio(event: any) { editAsset("setProperty", "unitRatio", parseFloat(event.target.value)); }
-function onChangeOpacityType(event: any) { editAsset("setProperty", "opacity", event.target.value === "transparent" ? 1 : null); }
+function onChangeUnitRatio(event: any) { data.projectClient.editAsset(SupClient.query.asset, "setProperty", "unitRatio", parseFloat(event.target.value)); }
+function onChangeOpacityType(event: any) { data.projectClient.editAsset(SupClient.query.asset, "setProperty", "opacity", event.target.value === "transparent" ? 1 : null); }
 function onChangeOpacity(event: any) {
   const opacity = parseFloat(event.target.value);
   if (isNaN(opacity)) return;
-  editAsset("setProperty", "opacity", opacity);
+  data.projectClient.editAsset(SupClient.query.asset, "setProperty", "opacity", opacity);
 }
 
 function onNewAnimationClick() {
@@ -271,7 +271,7 @@ function onNewAnimationClick() {
     /* tslint:enable:no-unused-expression */
     if (name == null) return;
 
-    editAsset("newAnimation", name, null, null, (animationId: string) => {
+    data.projectClient.editAsset(SupClient.query.asset, "newAnimation", name, null, null, (animationId: string) => {
       ui.animationsTreeView.clearSelection();
       ui.animationsTreeView.addToSelection(ui.animationsTreeView.treeRoot.querySelector(`li[data-id="${animationId}"]`) as HTMLLIElement);
       updateSelectedAnimation();
@@ -292,7 +292,7 @@ function onAnimationFileSelectChange(event: any) {
     if (data != null) {
       if (data.animation == null) { new SupClient.dialogs.InfoDialog("No animation found in imported files", SupClient.i18n.t("common:actions.close")); return; }
       // TODO: Check if bones are compatible
-      editAsset("setAnimation", animationId, data.animation.duration, data.animation.keyFrames);
+      data.projectClient.editAsset(SupClient.query.asset, "setAnimation", animationId, data.animation.duration, data.animation.keyFrames);
     }
   });
 }
@@ -313,7 +313,7 @@ function onRenameAnimationClick() {
     /* tslint:enable:no-unused-expression */
     if (newName == null) return;
 
-    editAsset("setAnimationProperty", animation.id, "name", newName);
+    data.projectClient.editAsset(SupClient.query.asset, "setAnimationProperty", animation.id, "name", newName);
   });
 }
 
@@ -327,7 +327,8 @@ function onDeleteAnimationClick() {
     /* tslint:enable:no-unused-expression */
     if (!confirm) return;
 
-    for (const selectedNode of ui.animationsTreeView.selectedNodes) editAsset("deleteAnimation", selectedNode.dataset["id"]);
+    for (const selectedNode of ui.animationsTreeView.selectedNodes)
+      data.projectClient.editAsset(SupClient.query.asset, "deleteAnimation", selectedNode.dataset["id"]);
   });
 }
 
@@ -336,7 +337,8 @@ function onAnimationsTreeViewDrop(event: DragEvent, dropLocation: TreeView.DropL
   for (const animation of orderedNodes) animationIds.push(animation.dataset["id"]);
 
   const index = SupClient.getListViewDropIndex(dropLocation, data.modelUpdater.modelAsset.animations);
-  for (let i = 0; i < animationIds.length; i++) editAsset("moveAnimation", animationIds[i], index + i);
+  for (let i = 0; i < animationIds.length; i++)
+    data.projectClient.editAsset(SupClient.query.asset, "moveAnimation", animationIds[i], index + i);
 
   return false;
 }
@@ -370,7 +372,7 @@ export function setupAnimation(animation: any, index: number) {
 function onEditMapSlot(event: any) {
   if (event.target.value !== "" && data.modelUpdater.modelAsset.pub.maps[event.target.value] == null) return;
   const slot = event.target.value !== "" ? event.target.value : null;
-  editAsset("setMapSlot", event.target.dataset["name"], slot);
+  data.projectClient.editAsset(SupClient.query.asset, "setMapSlot", event.target.dataset["name"], slot);
 }
 
 function onNewMapClick() {
@@ -384,7 +386,7 @@ function onNewMapClick() {
     /* tslint:enable:no-unused-expression */
     if (name == null) return;
 
-    editAsset("newMap", name);
+    data.projectClient.editAsset(SupClient.query.asset, "newMap", name);
   });
 }
 
@@ -397,7 +399,7 @@ function onMapFileSelectChange(event: any) {
   const maps: any = {};
   reader.onload = (event) => {
     maps[ui.selectedTextureName] = reader.result;
-    editAsset("setMaps", maps);
+    data.projectClient.editAsset(SupClient.query.asset, "setMaps", maps);
   };
 
   const element = <HTMLInputElement>event.target;
@@ -422,7 +424,7 @@ function onRenameMapClick() {
     /* tslint:enable:no-unused-expression */
     if (newName == null) return;
 
-    editAsset("renameMap", textureName, newName);
+    data.projectClient.editAsset(SupClient.query.asset, "renameMap", textureName, newName);
   });
 }
 
@@ -436,7 +438,8 @@ function onDeleteMapClick() {
     /* tslint:enable:no-unused-expression */
     if (!confirmed) return;
 
-    for (const selectedNode of ui.texturesTreeView.selectedNodes) editAsset("deleteMap", selectedNode.dataset["name"]);
+    for (const selectedNode of ui.texturesTreeView.selectedNodes)
+      data.projectClient.editAsset(SupClient.query.asset, "deleteMap", selectedNode.dataset["name"]);
   });
 }
 

plugins/default/cubicModel/editors/cubicModel/ui.ts
@@ -1,4 +1,4 @@
-import { data, editAsset } from "./network";
+import { data } from "./network";
 import engine, { setupHelpers } from "./engine";
 import { setSelectedNode as setTextureAreaSelectedNode } from "./textureArea";
 
@@ -111,7 +111,7 @@ function onGridVisibleChange(event: UIEvent) {
 ui.pixelsPerUnitInput = <HTMLInputElement>document.querySelector("input.property-pixelsPerUnit");
 ui.pixelsPerUnitInput.addEventListener("change", onChangePixelsPerUnit);
 
-function onChangePixelsPerUnit(event: any) { editAsset("setProperty", "pixelsPerUnit", parseFloat(event.target.value)); }
+function onChangePixelsPerUnit(event: any) { data.projectClient.editAsset(SupClient.query.asset, "setProperty", "pixelsPerUnit", parseFloat(event.target.value)); }
 
 // Texture download
 document.querySelector("button.download").addEventListener("click", (event) => {
@@ -157,8 +157,8 @@ for (let size of CubicModelAsset.validTextureSizes) {
   addOption(ui.textureWidthSelect, size);
   addOption(ui.textureHeightSelect, size);
 }
-ui.textureWidthSelect.addEventListener("input", (event: any) => { editAsset("changeTextureWidth", parseInt(event.target.value, 10)); });
-ui.textureHeightSelect.addEventListener("input", (event: any) => { editAsset("changeTextureHeight", parseInt(event.target.value, 10)); });
+ui.textureWidthSelect.addEventListener("input", (event: any) => { data.projectClient.editAsset(SupClient.query.asset, "changeTextureWidth", parseInt(event.target.value, 10)); });
+ui.textureHeightSelect.addEventListener("input", (event: any) => { data.projectClient.editAsset(SupClient.query.asset, "changeTextureHeight", parseInt(event.target.value, 10)); });
 
 // Setup tree view
 ui.treeViewElt = document.querySelector(".nodes-tree-view") as HTMLDivElement;
@@ -182,7 +182,7 @@ export function createNodeElement(node: Node) {
     event.stopPropagation();
     let { shape } = data.cubicModelUpdater.cubicModelRenderer.byNodeId[event.target.parentElement.dataset["id"]];
     shape.visible = !shape.visible;
-    visibleButton.textContent = SupClient.i18n.t(`cubicModelEditor:sidebar.nodes.${(shape.visible) ? "hide" : "show"}`)
+    visibleButton.textContent = SupClient.i18n.t(`cubicModelEditor:sidebar.nodes.${(shape.visible) ? "hide" : "show"}`);
     if (shape.visible) visibleButton.classList.add("show");
     else visibleButton.classList.remove("show");
   });
@@ -203,7 +203,7 @@ function onNodesTreeViewDrop(event: DragEvent, dropLocation: TreeView.DropLocati
 
   let i = 0;
   for (let id of nodeIds) {
-    editAsset("moveNode", id, dropPoint.parentId, dropPoint.index + i);
+    data.projectClient.editAsset(SupClient.query.asset, "moveNode", id, dropPoint.parentId, dropPoint.index + i);
     if (!sameParent || sourceChildren.indexOf(data.cubicModelUpdater.cubicModelAsset.nodes.byId[id]) >= dropPoint.index) i++;
   }
   return false;
@@ -454,7 +454,7 @@ function onNewNodeClick() {
       }
     };
 
-    editAsset("addNode", name, options, (nodeId: string) => {
+    data.projectClient.editAsset(SupClient.query.asset, "addNode", name, options, (nodeId: string) => {
       ui.nodesTreeView.clearSelection();
       ui.nodesTreeView.addToSelection(ui.nodesTreeView.treeRoot.querySelector(`li[data-id='${nodeId}']`) as HTMLLIElement);
       setupSelectedNode();
@@ -479,7 +479,7 @@ function onRenameNodeClick() {
     /* tslint:enable:no-unused-expression */
     if (newName == null) return;
 
-    editAsset("setNodeProperty", node.id, "name", newName);
+    data.projectClient.editAsset(SupClient.query.asset, "setNodeProperty", node.id, "name", newName);
   });
 }
 
@@ -500,9 +500,7 @@ function onDuplicateNodeClick() {
     if (newName == null) return;
     let options = SupClient.getTreeViewInsertionPoint(ui.nodesTreeView);
 
-    editAsset("duplicateNode", newName, node.id, options.index, (err: string, nodeId: string) => {
-      if (err != null) { new SupClient.dialogs.InfoDialog(err, SupClient.i18n.t("common:actions.close")); return; }
-
+    data.projectClient.editAsset(SupClient.query.asset, "duplicateNode", newName, node.id, options.index, (err: string, nodeId: string) => {
       ui.nodesTreeView.clearSelection();
       ui.nodesTreeView.addToSelection(ui.nodesTreeView.treeRoot.querySelector(`li[data-id='${nodeId}']`) as HTMLLIElement);
       setupSelectedNode();
@@ -521,7 +519,7 @@ function onDeleteNodeClick() {
     if (!confirm) return;
 
     for (let selectedNode of ui.nodesTreeView.selectedNodes) {
-      editAsset("removeNode", selectedNode.dataset["id"]);
+      data.projectClient.editAsset(SupClient.query.asset, "removeNode", selectedNode.dataset["id"]);
     }
   });
 }
@@ -569,9 +567,8 @@ function onInspectorInputChange(event: any) {
     }
   }
 
-  if (propertyType !== "position" || ui.translateMode !== "pivot") {
-    editAsset("setNodeProperty", nodeId, `${path}${propertyType}`, value);
-  } else {
-    editAsset("moveNodePivot", nodeId, value);
-  }
+  if (propertyType !== "position" || ui.translateMode !== "pivot")
+    data.projectClient.editAsset(SupClient.query.asset, "setNodeProperty", nodeId, `${path}${propertyType}`, value);
+  else
+    data.projectClient.editAsset(SupClient.query.asset, "moveNodePivot", nodeId, value);
 }

plugins/default/shader/editors/shader/ui.ts
@@ -1,4 +1,4 @@
-import { data, editAsset } from "./network";
+import { data } from "./network";
 import { setupPreview } from "./engine";
 import Uniforms, { UniformPub } from "../../data/Uniforms";
 import Attributes, { AttributePub } from "../../data/Attributes";
@@ -36,8 +36,8 @@ export function setupUniform(uniform: UniformPub) {
   let nameInputElt = document.createElement("input");
   nameInputElt.classList.add("name");
   nameInputElt.addEventListener("change", (event: any) => {
-    if (event.target.value === "") editAsset("deleteUniform", rowElt.dataset["id"]);
-    else editAsset("setUniformProperty", rowElt.dataset["id"], "name", event.target.value);
+    if (event.target.value === "") data.projectClient.editAsset(SupClient.query.asset, "deleteUniform", rowElt.dataset["id"]);
+    else data.projectClient.editAsset(SupClient.query.asset, "setUniformProperty", rowElt.dataset["id"], "name", event.target.value);
   });
   nameInputElt.value = uniform.name;
   nameElt.appendChild(nameInputElt);
@@ -52,7 +52,7 @@ export function setupUniform(uniform: UniformPub) {
   }
   selectTypeElt.classList.add("type");
   selectTypeElt.addEventListener("change", (event: any) => {
-    editAsset("setUniformProperty", rowElt.dataset["id"], "type", event.target.value);
+    data.projectClient.editAsset(SupClient.query.asset, "setUniformProperty", rowElt.dataset["id"], "type", event.target.value);
   });
   selectTypeElt.value = uniform.type;
   typeElt.appendChild(selectTypeElt);
@@ -77,7 +77,9 @@ export function setUniformValueInputs(id: string) {
       let floatInputElt = document.createElement("input");
       floatInputElt.type = "number";
       floatInputElt.classList.add("float");
-      floatInputElt.addEventListener("change", (event: any) => { editAsset("setUniformProperty", id, "value", parseFloat(event.target.value)); });
+      floatInputElt.addEventListener("change", (event: any) => {
+        data.projectClient.editAsset(SupClient.query.asset, "setUniformProperty", id, "value", parseFloat(event.target.value));
+      });
       floatInputElt.value = uniform.value;
       valueRowElt.appendChild(floatInputElt);
       break;
@@ -92,7 +94,9 @@ export function setUniformValueInputs(id: string) {
     case "t":
       let textInputElt = document.createElement("input");
       textInputElt.classList.add("text");
-      textInputElt.addEventListener("change", (event: any) => { editAsset("setUniformProperty", id, "value", event.target.value); });
+      textInputElt.addEventListener("change", (event: any) => {
+        data.projectClient.editAsset(SupClient.query.asset, "setUniformProperty", id, "value", event.target.value);
+      });
       textInputElt.value = uniform.value;
       valueRowElt.appendChild(textInputElt);
       break;
@@ -112,7 +116,7 @@ function setArrayUniformInputs(id: string, parentElt: HTMLDivElement, name: stri
         let elt = <HTMLInputElement>parentElt.querySelector(`.${name}_${j}`);
         values.push(parseFloat(elt.value));
       }
-      editAsset("setUniformProperty", id, "value", values);
+      data.projectClient.editAsset(SupClient.query.asset, "setUniformProperty", id, "value", values);
     });
     inputElt.value = uniform.value[i];
     parentElt.appendChild(inputElt);
@@ -122,14 +126,14 @@ function setArrayUniformInputs(id: string, parentElt: HTMLDivElement, name: stri
 let newUniformInput = <HTMLInputElement>document.querySelector(".uniforms .new input");
 newUniformInput.addEventListener("keyup", (event: any) => {
   if (event.keyCode === 13) {
-    editAsset("newUniform", event.target.value);
+    data.projectClient.editAsset(SupClient.query.asset, "newUniform", event.target.value);
     event.target.value = "";
   }
 });
 
 ui.useLightUniformsCheckbox = <HTMLInputElement>document.getElementById("use-light-uniforms");
 ui.useLightUniformsCheckbox.addEventListener("change", (event: any) => {
-  editAsset("setProperty", "useLightUniforms", event.target.checked);
+  data.projectClient.editAsset(SupClient.query.asset, "setProperty", "useLightUniforms", event.target.checked);
 });
 
 ui.attributesList = <HTMLTableElement>document.querySelector(".attributes tbody");
@@ -142,8 +146,8 @@ export function setupAttribute(attribute: AttributePub) {
   let nameInputElt = document.createElement("input");
   nameInputElt.classList.add("name");
   nameInputElt.addEventListener("change", (event: any) => {
-    if (event.target.value === "") editAsset("deleteAttribute", rowElt.dataset["id"]);
-    else editAsset("setAttributeProperty", rowElt.dataset["id"], "name", event.target.value);
+    if (event.target.value === "") data.projectClient.editAsset(SupClient.query.asset, "deleteAttribute", rowElt.dataset["id"]);
+    else data.projectClient.editAsset(SupClient.query.asset, "setAttributeProperty", rowElt.dataset["id"], "name", event.target.value);
   });
   nameInputElt.value = attribute.name;
   nameElt.appendChild(nameInputElt);
@@ -157,7 +161,7 @@ export function setupAttribute(attribute: AttributePub) {
     selectTypeElt.appendChild(optionElt);
   }
   selectTypeElt.classList.add("type");
-  selectTypeElt.addEventListener("change", (event: any) => { editAsset("setAttributeProperty", rowElt.dataset["id"], "type", event.target.value); });
+  selectTypeElt.addEventListener("change", (event: any) => { data.projectClient.editAsset(SupClient.query.asset, "setAttributeProperty", rowElt.dataset["id"], "type", event.target.value); });
   selectTypeElt.value = attribute.type;
   typeElt.appendChild(selectTypeElt);
   rowElt.appendChild(typeElt);
@@ -170,7 +174,7 @@ export function setupAttribute(attribute: AttributePub) {
 let newAttributeInput = <HTMLInputElement>document.querySelector(".attributes .new input");
 newAttributeInput.addEventListener("keyup", (event: any) => {
   if (event.keyCode === 13) {
-    editAsset("newAttribute", event.target.value);
+    data.projectClient.editAsset(SupClient.query.asset, "newAttribute", event.target.value);
     event.target.value = "";
   }
 });
@@ -183,12 +187,11 @@ shaderPaneResizeHandle.on("drag", () => {
 });
 
 function onSaveVertex() {
-  // if (!ui.vertexHeader.classList.contains("has-errors")) editAsset("saveVertexShader");
-  editAsset("saveVertexShader");
+  if (!ui.vertexHeader.classList.contains("has-errors")) data.projectClient.editAsset(SupClient.query.asset, "saveVertexShader");
 }
 
 function onSaveFragment() {
-  if (!ui.fragmentHeader.classList.contains("has-errors")) editAsset("saveFragmentShader");
+  if (!ui.fragmentHeader.classList.contains("has-errors")) data.projectClient.editAsset(SupClient.query.asset, "saveFragmentShader");
 }
 
 let fragmentShadersPane = shadersPane.querySelector(".fragment");
@@ -214,7 +217,7 @@ export function setupEditors(clientId: number) {
       "Cmd-S": () => { onSaveVertex(); },
     },
     sendOperationCallback: (operation: OperationData) => {
-      editAsset("editVertexShader", operation, data.shaderAsset.vertexDocument.getRevisionId());
+      data.projectClient.editAsset(SupClient.query.asset, "editVertexShader", operation, data.shaderAsset.vertexDocument.getRevisionId());
     }
   });
 
@@ -226,7 +229,7 @@ export function setupEditors(clientId: number) {
       "Cmd-S": () => { onSaveFragment(); },
     },
     sendOperationCallback: (operation: OperationData) => {
-      editAsset("editFragmentShader", operation, data.shaderAsset.fragmentDocument.getRevisionId());
+      data.projectClient.editAsset(SupClient.query.asset, "editFragmentShader", operation, data.shaderAsset.fragmentDocument.getRevisionId());
     }
   });
 }

commit ff3b1b64b642bbf3ff9a513c744778d68998b08b

model: Fix importing models and animations
Broken in 66379222881924770b404f99ba29978990b8c7fb

plugins/default/model/editors/model/ui.ts
@@ -193,14 +193,14 @@ function onModelFileSelectChange(event: any) {
 
   ui.errorsTBody.innerHTML = "";
 
-  importModel(event.target.files, (log, data) => {
+  importModel(event.target.files, (log, result) => {
     event.target.parentElement.reset();
 
     setImportLog(log);
 
-    if (data != null) {
-      data.projectClient.editAsset(SupClient.query.asset, "setModel", data.upAxisMatrix, data.attributes, data.bones);
-      if (data.maps != null) data.projectClient.editAsset(SupClient.query.asset, "setMaps", data.maps);
+    if (result != null) {
+      data.projectClient.editAsset(SupClient.query.asset, "setModel", result.upAxisMatrix, result.attributes, result.bones);
+      if (result.maps != null) data.projectClient.editAsset(SupClient.query.asset, "setMaps", result.maps);
     }
   });
 }
@@ -284,20 +284,20 @@ function onAnimationFileSelectChange(event: any) {
 
   const animationId: string = ui.selectedAnimationId;
 
-  importModel(event.target.files, (log, data) => {
+  importModel(event.target.files, (log, result) => {
     event.target.parentElement.reset();
 
     setImportLog(log);
 
     if (data != null) {
-      if (data.animation == null) {
+      if (result.animation == null) {
         /* tslint:disable:no-unused-expression */
         new SupClient.Dialogs.InfoDialog("No animation found in imported files");
         /* tslint:enable:no-unused-expression */
         return;
       }
       // TODO: Check if bones are compatible
-      data.projectClient.editAsset(SupClient.query.asset, "setAnimation", animationId, data.animation.duration, data.animation.keyFrames);
+      data.projectClient.editAsset(SupClient.query.asset, "setAnimation", animationId, result.animation.duration, result.animation.keyFrames);
     }
   });
 }