c794af881b4809592ff394ce2aa2bfd64a42699b
Elisée Maurer <elisee@sparklinlabs.com>
2016-04-19 04:14:39 +0200

    font: Fix opacity at runtime

--- plugins/default/font/runtime/TextRenderer.ts
+++ plugins/default/font/runtime/TextRenderer.ts
@@ -1,8 +1,11 @@
 export function setupComponent(player: SupRuntime.Player, component: any, config: any) {
   component.setText(config.text);
-  component.setOptions({ alignment: config.alignment, verticalAlignment: config.verticalAlignment, size: config.size, color: config.color, opacity: config.opacity });
+  component.setOptions({ alignment: config.alignment, verticalAlignment: config.verticalAlignment, size: config.size, color: config.color });
+  if (config.overrideOpacity) component.opacity = config.opacity;
+
   if (config.fontAssetId != null) {
-    let font = player.getOuterAsset(config.fontAssetId).__inner;
+    const font = player.getOuterAsset(config.fontAssetId).__inner;
+    if (!config.overrideOpacity) component.opacity = font.opacity;
     component.setFont(font);
   }
 }
