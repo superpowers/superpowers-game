e0790fdc3ca3c08c2cd875f9f5aacdc982b5d0a5
bilou84 <nicolas.gauthier84@gmail.com>
2016-07-02 02:10:16 +0200

    gameSettings: Check if output folder is empty when exporting

--- plugins/default/gameSettings/build/GameBuildSettingsEditor.ts
+++ plugins/default/gameSettings/build/GameBuildSettingsEditor.ts
@@ -3,9 +3,10 @@ import * as TreeView from "dnd-tree-view";
 let outputFolder: string;
 
 export default class GameBuildSettingsEditor implements SupClient.BuildSettingsEditor {
-  private outputFolder = outputFolder;
   private outputFolderTextfield: HTMLInputElement;
   private outputFolderButton: HTMLButtonElement;
+  private errorRowElt: HTMLTableRowElement;
+  private errorInput: HTMLInputElement;
 
   private table: HTMLTableElement;
 
@@ -15,31 +16,81 @@ export default class GameBuildSettingsEditor implements SupClient.BuildSettingsE
     table.classList.add("properties");
     table.hidden = true;
 
-    {
-      const row = SupClient.table.appendRow(tbody, SupClient.i18n.t("buildSettingsEditors:game.outputFolder"));
-      const inputs = SupClient.html("div", "inputs", { parent: row.valueCell });
+    const outputFolderRow = SupClient.table.appendRow(tbody, SupClient.i18n.t("buildSettingsEditors:game.outputFolder"));
+    const inputs = SupClient.html("div", "inputs", { parent: outputFolderRow.valueCell });
 
-      const value = this.outputFolder != null ? this.outputFolder : "";
-      this.outputFolderTextfield = SupClient.html("input", { parent: inputs, type: "text", value, readOnly: true, style: { cursor: "pointer" } }) as HTMLInputElement;
-      this.outputFolderButton = SupClient.html("button", { parent: inputs, textContent: SupClient.i18n.t("common:actions.select") }) as HTMLButtonElement;
+    const value = outputFolder != null ? outputFolder : "";
+    this.outputFolderTextfield = SupClient.html("input", { parent: inputs, type: "text", value, readOnly: true, style: { cursor: "pointer" } }) as HTMLInputElement;
+    this.outputFolderButton = SupClient.html("button", { parent: inputs, textContent: SupClient.i18n.t("common:actions.select") }) as HTMLButtonElement;
 
-      this.outputFolderTextfield.addEventListener("click", (event) => { event.preventDefault(); this.selectOutputfolder(); });
-      this.outputFolderButton.addEventListener("click", (event) => { event.preventDefault(); this.selectOutputfolder(); });
-    }
+    this.outputFolderTextfield.addEventListener("click", (event) => { event.preventDefault(); this.selectOutputfolder(); });
+    this.outputFolderButton.addEventListener("click", (event) => { event.preventDefault(); this.selectOutputfolder(); });
+
+    const errorRow = SupClient.table.appendRow(tbody, "Error");
+    this.errorRowElt = errorRow.row;
+    this.errorRowElt.hidden = true;
+    this.errorInput = SupClient.html("input", { parent: errorRow.valueCell, type: "text", readOnly: true }) as HTMLInputElement;
   }
 
   setVisible(visible: boolean) {
     this.table.hidden = !visible;
   }
 
-  getSettings(): GameBuildSettings {
-    return { outputFolder: this.outputFolder };
+  getSettings(callback: (settings: GameBuildSettings) => void) {
+    this.ensureOutputFolderValid((outputFolderValid) => {
+      callback(outputFolderValid ? { outputFolder } : null);
+    });
   }
 
   private selectOutputfolder() {
-    SupApp.chooseFolder((folder) => {
-      this.outputFolder = folder;
-      this.outputFolderTextfield.value = (folder != null) ? folder : "";
+    SupApp.chooseFolder((folderPath) => {
+      if (folderPath == null) return;
+
+      outputFolder = this.outputFolderTextfield.value = folderPath;
+      this.ensureOutputFolderValid();
     });
+  }
+
+  private ensureOutputFolderValid(callback?: (outputFolderValid: boolean) => void) {
+    if (outputFolder == null) {
+      this.displayError(SupClient.i18n.t("buildSettingsEditors:game.errors.selectDestionationFolder"));
+      if (callback != null) callback(false);
+      return;
+    }
+
+    SupApp.readDir(outputFolder, (err, files) => {
+      if (err != null) {
+        this.displayError(SupClient.i18n.t("buildSettingsEditors:game.errors.emptyDirectoryCheckFail"));
+        console.log(err);
+        if (callback != null) callback(false);
+        return;
+      }
+
+      let index = 0;
+      while (index < files.length) {
+        const item = files[index];
+        if (item[0] === "." || item === "Thumbs.db") files.splice(index, 1);
+        else index++;
+      }
+
+      if (files.length > 0) {
+        this.displayError(SupClient.i18n.t("buildSettingsEditors:game.errors.destinationFolderEmpty"));
+        if (callback != null) callback(false);
+      } else {
+        this.errorRowElt.hidden = true;
+        if (callback != null) callback(true);
+      }
+    });
+  }
+
+  private displayError(err: string) {
+    this.errorRowElt.hidden = false;
+    this.errorInput.value = err;
+
+    (this.errorRowElt as any).animate([
+      { transform: "translateX(0)" },
+      { transform: "translateX(1.5vh)" },
+      { transform: "translateX(0)" }
+    ], { duration: 100 });
   }
 }
