4521d3cfe2f3ab59f1382900a3dec2e5688d90e2
bilou84 <nicolas.gauthier84@gmail.com>
2016-09-03 18:30:49 +0200

    p2js: Make scene marker not affected by actor scale and rotation along x and y since it doesn't affect the body, closes #98

--- plugins/extra/p2js/components/P2BodyMarker.ts
+++ plugins/extra/p2js/components/P2BodyMarker.ts
@@ -1,21 +1,39 @@
-let THREE = SupEngine.THREE;
 import P2BodyMarkerUpdater from "./P2BodyMarkerUpdater";
+
+let THREE = SupEngine.THREE;
+let tmpVector3 = new THREE.Vector3();
+let tmpEulerAngles = new THREE.Euler();
 
 export default class P2BodyMarker extends SupEngine.ActorComponent {
   /* tslint:disable:variable-name */
   static Updater = P2BodyMarkerUpdater;
   /* tslint:enable:variable-name */
 
+  markerActor: SupEngine.Actor;
   mesh: THREE.Line|THREE.Mesh;
   offset = new THREE.Vector3(0, 0, 0);
   angle = 0;
 
   constructor(actor: SupEngine.Actor) {
     super(actor, "P2BodyMarker");
+
+    this.markerActor = new SupEngine.Actor(this.actor.gameInstance, `Marker`, null, { layer: -1 });
   }
 
   setIsLayerActive(active: boolean) {
     if (this.mesh != null) this.mesh.visible = active;
+  }
+
+  update() {
+    super.update();
+
+    this.actor.getGlobalPosition(tmpVector3);
+    this.markerActor.setGlobalPosition(tmpVector3);
+
+    this.actor.getGlobalEulerAngles(tmpEulerAngles);
+    tmpEulerAngles.x = tmpEulerAngles.y = 0;
+    tmpEulerAngles.z += this.angle;
+    this.markerActor.setGlobalEulerAngles(tmpEulerAngles);
   }
 
   setBox(width: number, height: number) {
@@ -31,7 +49,7 @@ export default class P2BodyMarker extends SupEngine.ActorComponent {
     );
     let material = new THREE.LineBasicMaterial({ color: 0xf459e4 });
     this.mesh = new THREE.Line(geometry, material);
-    this.actor.threeObject.add(this.mesh);
+    this.markerActor.threeObject.add(this.mesh);
     this.mesh.position.copy(this.offset);
     this.mesh.rotation.z = this.angle;
     this.mesh.updateMatrixWorld(false);
@@ -43,7 +61,7 @@ export default class P2BodyMarker extends SupEngine.ActorComponent {
     let geometry = new THREE.CircleGeometry(radius, 16);
     let material = new THREE.MeshBasicMaterial({ color: 0xf459e4, wireframe: true });
     this.mesh = new THREE.Mesh(geometry, material);
-    this.actor.threeObject.add(this.mesh);
+    this.markerActor.threeObject.add(this.mesh);
     this.mesh.position.copy(this.offset);
     this.mesh.updateMatrixWorld(false);
   }
@@ -61,7 +79,7 @@ export default class P2BodyMarker extends SupEngine.ActorComponent {
   }
 
   _clearRenderer() {
-    this.actor.threeObject.remove(this.mesh);
+    this.markerActor.threeObject.remove(this.mesh);
     this.mesh.geometry.dispose();
     this.mesh.material.dispose();
     this.mesh = null;
@@ -69,6 +87,7 @@ export default class P2BodyMarker extends SupEngine.ActorComponent {
 
   _destroy() {
     if (this.mesh != null) this._clearRenderer();
+    this.actor.gameInstance.destroyActor(this.markerActor);
     super._destroy();
   }
 }
